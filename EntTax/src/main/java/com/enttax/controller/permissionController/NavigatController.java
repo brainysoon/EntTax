2f77fff3788276e21103b3766dba7775beda635d
lcyanxi <845892601@qq.com>
2017-03-19 09:40:42 +0800

    add-imagecode

---
+++ EntTax/src/main/java/com/enttax/controller/permissionController/NavigatController.java
@@ -0,0 +1,7 @@
+package com.enttax.controller.permissionController;
+
+/**
+ * Created by lcyanxi on 17-3-19.
+ */
+public class NavigatController {
+}
---
+++ EntTax/src/main/java/com/enttax/util/constant/ConstantException.java
@@ -0,0 +1,7 @@
+package com.enttax.util.constant;
+
+/**
+ * Created by lcyanxi on 17-3-17.
+ */
+public class ConstantException {
+}
