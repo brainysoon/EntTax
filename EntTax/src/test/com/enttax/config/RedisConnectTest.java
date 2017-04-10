8d3887101b9f2eda42fdda9ff92819e942669452
lcyanxi <845892601@qq.com>
2017-04-10 20:58:40 +0800

    add-test-java

--- EntTax/src/test/com/enttax/config/RedisConnectTest.java
+++ EntTax/src/test/com/enttax/config/RedisConnectTest.java
@@ -1,7 +1,20 @@
 package com.enttax.config;
 
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
 /**
  * Created by lcyanxi on 17-3-30.
  */
 public class RedisConnectTest {
+    @Test
+    public void redisConnectTest(){
+        Jedis jedis=new Jedis("127.0.0.1",6379);
+        jedis.set("username","lichang");
+        jedis.set("password","1214521");
+        jedis.set("address","重庆黔江");
+        System.out.println(jedis.get("username"));
+        System.out.println(jedis.keys("*"));
+
+    }
 }
