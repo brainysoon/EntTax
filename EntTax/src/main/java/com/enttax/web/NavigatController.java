056b031ba7170d541f6f9636d84159dc6c294321
brainysoon <brainysoon@gmail.com>
2017-04-15 17:11:35 +0800

    merge-font-and-back-together-base-login

--- EntTax/src/main/java/com/enttax/web/NavigatController.java
+++ EntTax/src/main/java/com/enttax/web/NavigatController.java
@@ -4,10 +4,10 @@ import com.enttax.model.Staff;
 import com.enttax.util.constant.ConstantException;
 import com.enttax.util.constant.ConstantStr;
 import com.enttax.util.tools.ToolImageCode;
-import com.enttax.util.tools.ToolSendSms;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,14 +23,27 @@ import java.util.Map;
 public class NavigatController extends BaseController {
     /**
      * 产生随机图片验证码
-     * @return
+     *
      * @throws IOException
      * @throws ServletException
      */
     @RequestMapping(value = "/captcha")
-    public String index() throws IOException,ServletException{
-        ToolImageCode.doPost(request,response);
-        return "index";
+    public void captcha() throws IOException, ServletException {
+        ToolImageCode.doPost(request, response);
+    }
+
+    /**
+     * 判断验证码是否正确
+     *
+     * @param kcode 验证码
+     * @throws IOException
+     */
+    @RequestMapping(value = "/checkcaptcha", method = RequestMethod.POST)
+    public void checkCaptcha(@RequestParam("kcode") String kcode) throws IOException {
+
+        //判断验证码是否正确，并将结果输出 这里去除了大小写铭感
+        response.getWriter().print(kcode.toLowerCase()
+                .equals(request.getSession().getAttribute(ConstantStr.SRAND)));
     }
 
 //    @RequestMapping(value = "/sendSMS")
@@ -49,73 +62,70 @@ public class NavigatController extends BaseController {
 //    }
 
     /**
-     *
      * @param numcode
      * @return
      */
     @RequestMapping(value = "/validacode")
     @ResponseBody
-    public Map<String,String> validacode(@RequestParam("numcode") String numcode){
+
+    public Map<String, String> validacode(@RequestParam("numcode") String numcode) {
 //        @RequestParam("code") String code,
 //        System.out.println(code+numcode);
-        Map<String,String> map=new HashMap<String, String>();
+        Map<String, String> map = new HashMap<String, String>();
 //        String sRand = (String) request.getSession().getAttribute(ConstantStr.SRAND);
-        String smsCode= (String) request.getSession().getAttribute(ConstantStr.SMSCODE);
+        String smsCode = (String) request.getSession().getAttribute(ConstantStr.SMSCODE);
 //        code.equals(sRand) &&
-        if ( numcode.equals(smsCode)){
+        if (numcode.equals(smsCode)) {
             map.put(ConstantStr.STATUS, ConstantStr.str_one);
-        }else {
-            map.put(ConstantStr.STATUS,ConstantStr.str_zero);
-            map.put(ConstantStr.MESSAGE,ConstantException.smscode_error_message);
+        } else {
+            map.put(ConstantStr.STATUS, ConstantStr.str_zero);
+            map.put(ConstantStr.MESSAGE, ConstantException.smscode_error_message);
         }
 
         return map;
-
     }
 
     /**
      * 导航到重置密码页面
+     *
      * @return
      */
     @RequestMapping(value = "/updatepassword")
-    public String updatepasswordPage(){
+    public String updatepasswordPage() {
         return "updatepassword";
     }
 
     /**
      * 导航到主页面
+     *
      * @return
      */
     @RequestMapping(value = "/home")
-    public String mainPage(Model model){
-       Staff staff=(Staff) session.getAttribute(ConstantStr.STAFFINFO);
-       model.addAttribute(staff);
+    public String mainPage(Model model) {
+        Staff staff = (Staff) session.getAttribute(ConstantStr.STAFFINFO);
+        model.addAttribute(staff);
         return "home";
     }
 
     /**
      * 导航到找回密码页面
+     *
      * @return
      */
     @RequestMapping(value = "/findpassword")
-    public String findpasswordPage(){
+    public String findpasswordPage() {
         return "findpassword";
     }
 
     /**
      * 安全退出
+     *
      * @return
      */
     @RequestMapping(value = "/loginOut")
-    public String loginOut(){
+    public String loginOut() {
         session.removeAttribute(ConstantStr.STAFFINFO);
         session.invalidate();
         return "redirect:/index";
     }
-
-    @RequestMapping(value = "/")
-    public void indexTest(){
-        System.out.println("hello world");
-    }
-
 }
