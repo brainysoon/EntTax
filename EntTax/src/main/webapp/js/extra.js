153301b9cfb74797e33d8ff3f11d776e634a0012
Hxmic <mic7512@163.com>
2017-05-25 08:51:34 +0800

    add inform.html and extra.css/js

---
+++ EntTax/src/main/webapp/js/extra.js
@@ -0,0 +1,31 @@
+/**
+ * Created by hxmic on 17-5-25.
+ */
+
+/*用于设置清空消息列表,和标记消息*/
+function sampleAll(){
+    var obj = document.getElementById('mark-read');
+    var allread = document.getElementById('all-read');
+    var clearinfo = document.getElementById('clear-infor');
+    allread.style.display = "none";
+    obj.style.display = "none";
+    clearinfo.style.display = "block";
+}
+/*用于设置清空消息列表,和标记消息*/
+function sampleNone() {
+    var obj = document.getElementById('mark-read');
+    var allread = document.getElementById('all-read');
+    var clearinfo = document.getElementById('clear-infor');
+    allread.style.display = "none";
+    obj.style.display = "block";
+    clearinfo.style.display = "block";
+}
+/*用于设置清空消息列表,和标记消息*/
+function sampleRead() {
+    var obj = document.getElementById('mark-read');
+    var allread = document.getElementById('all-read');
+    var clearinfo = document.getElementById('clear-infor');
+    obj.style.display = "none"
+    allread.style.display = "block";
+    clearinfo.style.display = "none"
+}
